7f83691c44c69e4737cd3b11c2ed53a67470aa12
Mitsu23 <aza.mitsu.23@gmail.com>
2025-10-22 11:30:37 +0900

    fix: デモページ修正

--- demo/js/script.js
+++ demo/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       '../../source/js/main.js'
     );
 
-    // JUNO type1
+    // 【JUNO-1】二重・クマ・糸・小顔・女性泌尿器
     const embedClinicMapInstance1 = embedClinicMap({
       parentSelector: '#clinic-list-1',
       clinicType: 'juno-1',
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
     embedClinicMapInstance1.init();
 
-    // JUNO type2
+    // 【JUNO-2】スルリム
     const embedClinicMapInstance2 = embedClinicMap({
       parentSelector: '#clinic-list-2',
       clinicType: 'juno-2',
@@ -21,45 +21,45 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
     embedClinicMapInstance2.init();
 
-    // JUNO type3
-    const embedClinicMapInstance6 = embedClinicMap({
-      parentSelector: '#clinic-list-6',
+    // 【JUNO-3】冷却
+    const embedClinicMapInstance3 = embedClinicMap({
+      parentSelector: '#clinic-list-3',
       clinicType: 'juno-3',
       colors: { mainColor: '#ffc157', subColor: '#fff9deff' },
     });
-    embedClinicMapInstance6.init();
+    embedClinicMapInstance3.init();
 
-    // JUNO type4
-    const embedClinicMapInstance7 = embedClinicMap({
-      parentSelector: '#clinic-list-7',
+    // 【JUNO-4】AGA(女性)
+    const embedClinicMapInstance4 = embedClinicMap({
+      parentSelector: '#clinic-list-4',
       clinicType: 'juno-4',
       colors: { mainColor: '#6be080ff', subColor: '#deffdfff' },
     });
-    embedClinicMapInstance7.init();
+    embedClinicMapInstance4.init();
 
-    // ATOM type1
-    const embedClinicMapInstance3 = embedClinicMap({
-      parentSelector: '#clinic-list-3',
+    // 【ATOM-1】AGA(男性)
+    const embedClinicMapInstance5 = embedClinicMap({
+      parentSelector: '#clinic-list-5',
       clinicType: 'atom-1',
       colors: { mainColor: '#7eb8fd', subColor: '#cee5ff' },
     });
-    embedClinicMapInstance3.init();
+    embedClinicMapInstance5.init();
 
-    // ATOM type2
-    const embedClinicMapInstance4 = embedClinicMap({
-      parentSelector: '#clinic-list-4',
+    // 【ATOM-2】泌尿器、ED
+    const embedClinicMapInstance6 = embedClinicMap({
+      parentSelector: '#clinic-list-6',
       clinicType: 'atom-2',
       colors: { mainColor: '#7e7efdff', subColor: '#ced0ffff' },
     });
-    embedClinicMapInstance4.init();
+    embedClinicMapInstance6.init();
 
-    // BeYOU type1
-    const embedClinicMapInstance5 = embedClinicMap({
-      parentSelector: '#clinic-list-5',
+    // 【BeYOU-1】
+    const embedClinicMapInstance7 = embedClinicMap({
+      parentSelector: '#clinic-list-7',
       clinicType: 'beyou-1',
       colors: { mainColor: '#dfce6aff', subColor: '#fff8ceff' },
     });
-    embedClinicMapInstance5.init();
+    embedClinicMapInstance7.init();
   } catch (e) {
     console.error(e);
   } finally {
